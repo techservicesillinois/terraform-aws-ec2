c4da2b4711bd88aa86808a9f20de7e0b481a339c
Jon R. Roma <roma@illinois.edu>
2019-09-17 15:44:17 -0500

    Minor enhancements
    *   Add support for forcing AZ (such as when EBS volumes
        need to be mounted).
    
    *   Add defaults to make EFS block optional.

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "default" {
   ami                         = "${data.aws_ami.selected.id}"
   instance_type               = "${var.instance_type}"
-  availability_zone           = "${var.availability_zone}"
+  availability_zone           = "${local.availability_zone}"
   key_name                    = "${var.key_name}"
   vpc_security_group_ids      = ["${concat(list(aws_security_group.default.id), flatten(data.aws_security_groups.selected.*.ids))}"]
   subnet_id                   = "${local.subnet_id}"
